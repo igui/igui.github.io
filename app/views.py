1747f3987e7c1674104ec5cc87a335f622a613a0
Ignacio Avas <gmail@ignacioavas.com>
2017-03-21 00:41:09 -0300

    Dynamic images and age

--- app/views.py
+++ app/views.py
@@ -1,8 +1,26 @@
+"App Views"
+import random
+from datetime import datetime
 from django.shortcuts import render
-from django.http import HttpResponse
-from django_pyscss import DjangoScssCompiler
 
 # Create your views here.
 def index(request):
     "Return the index file"
-    return render(request, 'index.html')
+    days_per_year = 365.2425
+    birth = datetime(1987, 6, 15)
+    age = int((datetime.now() - birth).days / days_per_year)
+
+    jumbotron_image = random.choice([
+        '/static/images/jumbotron-1.jpg',
+        '/static/images/jumbotron-2.jpg',
+        '/static/images/jumbotron-3.jpg',
+        '/static/images/jumbotron-4.jpg'
+    ])
+
+    context = {
+        'age': age,
+        'jumbotron': jumbotron_image
+    }
+
+    return render(request, 'index.html', context)
+
