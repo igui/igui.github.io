c898da1af05622c82d686d4a0777ed5f1a5b4d23
Ignacio Avas <gmail@ignacioavas.com>
2017-03-27 01:16:17 -0300

    Run local python server on gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
-var minifycss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var livereload = require('gulp-livereload');
-var autoprefixer = require('gulp-autoprefixer');
+/* jshint esversion: 6 */
+/* jshint node: true */
+'use strict';
 
-var gzip_options = {
-    threshold: '1kb',
-    gzipOptions: {
-        level: 9
-    }
-};
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+const minifycss = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const livereload = require('gulp-livereload');
+const autoprefixer = require('gulp-autoprefixer');
+const spawn = require('child_process').spawn;
+
 
 /* Compile Our Sass */
-gulp.task('sass', function() {
+gulp.task('sass', () => {
     return gulp.src('app/scss/styles.scss')
         .pipe(sass())
         .pipe(autoprefixer())
@@ -24,13 +23,22 @@ gulp.task('sass', function() {
 });
 
 /* Watch Files For Changes */
-gulp.task('watch', function() {
+gulp.task('watch', () => {
     livereload.listen();
     gulp.watch('app/scss/**/*.scss', ['sass']);
 
     /* Trigger a live reload on any Django template changes */
     gulp.watch('app/templates/**/*.html').on('change', livereload.changed);
-
 });
 
-gulp.task('default', ['sass', 'watch']);
+/* Run local server */
+gulp.task('runserver', () => {
+    const runserver = spawn('heroku', 
+        ['local', 'web', '-f', 'Procfile.windows'],
+        {
+            stdio: 'inherit',
+            shell: true
+        });
+});
+
+gulp.task('default', ['sass', 'watch', 'runserver']);
