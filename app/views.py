5bff860f5f8634bd8ad78ad2edb0bf43745a7a54
Ignacio Avas <gmail@ignacioavas.com>
2017-03-29 00:14:52 -0300

    Try to have a video in the jumbotron

--- app/views.py
+++ app/views.py
@@ -10,16 +10,8 @@ def index(request):
     birth = datetime(1987, 6, 15)
     age = int((datetime.now() - birth).days / days_per_year)
 
-    jumbotron_image = random.choice([
-        '/static/images/jumbotron-1.jpg',
-        '/static/images/jumbotron-2.jpg',
-        '/static/images/jumbotron-3.jpg',
-        '/static/images/jumbotron-4.jpg'
-    ])
-
     context = {
-        'age': age,
-        'jumbotron': jumbotron_image
+        'age': age
     }
 
     return render(request, 'index.html', context)
