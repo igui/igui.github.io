b6ebc73df5c2c72fb903244d69e285b3577fdf3a
Ignacio Avas <gmail@ignacioavas.com>
2017-04-17 23:08:43 -0300

    Remove unnecessary files Move timeline to template, as it is embedded to allow text selection Use variables for sizing

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@
 import random
 from datetime import datetime
 from django.shortcuts import render
+from django.template import loader, Context
+import os.path
 
 # Create your views here.
 def index(request):
@@ -10,8 +12,12 @@ def index(request):
     birth = datetime(1987, 6, 15)
     age = int((datetime.now() - birth).days / days_per_year)
 
+    timeline_template = loader.get_template('timeline.svg')
+    timeline_rendered = timeline_template.render(Context({}))
+
     context = {
-        'age': age
+        'age': age,
+        'timeline': timeline_rendered
     }
 
     return render(request, 'index.html', context)
