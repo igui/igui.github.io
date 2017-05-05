dbafd8716c52196630d4b3359a0865687a562e5b
Ignacio Avas <gmail@ignacioavas.com>
2017-05-05 02:40:25 -0300

    Rethinked timeline using SVG sparingly

--- app/views.py
+++ app/views.py
@@ -12,14 +12,10 @@ def index(request):
     birth = datetime(1987, 6, 15)
     age = int((datetime.now() - birth).days / days_per_year)
 
-    timeline_template = loader.get_template('timeline.svg')
-    timeline_rendered = timeline_template.render(Context({}))
-
     cv_url = 'https://www.dropbox.com/s/l2a7xfibomb4a59/CV%20Ignacio%20Avas.pdf?dl=1'
 
     context = {
         'age': age,
-        'timeline': timeline_rendered,
         'cv_url': cv_url
     }
 
